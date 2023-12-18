22c1102e8e259d26c578ea395cf510d0a0bad5a6
Dmitriy <altusj@gmail.com>
2023-12-18 18:01:43 -0500

    add orders from db to show in index.html

--- webcrm/crm/models.py
+++ webcrm/crm/models.py
@@ -4,3 +4,6 @@ class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200)
     order_phone = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.order_name
